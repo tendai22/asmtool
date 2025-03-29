74c2e5883d0eb265e23060b38d3512761d56f828
Norihiro Kumagai <tendai22plus@gmail.com>
2025-03-29 12:16:17 +0900

    conv.sh: now convert result can pass "asz80 -ls emubasic.asm"

--- conv.sh
+++ conv.sh
@@ -4,4 +4,7 @@ sh c1.sh | tee x1.log |
 sh c2.sh | tee x2.log |
 sh c3.sh | tee x3.log |
 sh c4.sh | tee x4.log |
-sh c5.sh | tee x5.log > /dev/null
+sh c5.sh | tee x5.log |(
+    echo "        .area TEST (ABS)"
+    cat
+)
