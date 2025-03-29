31faa3c1cc5bd948db40fb37bc26d2ca20daeaec
Norihiro Kumagai <tendai22plus@gmail.com>
2025-03-29 12:04:37 +0900

    c1.sh,c5.sh: secure to put trailing semicolon(:) at label and remove semicolon in .equ line

--- c5.sh
+++ c5.sh
@@ -1,5 +1,15 @@
 #! /bin/sh
 sed '
+# remove trailing : in EQU line
+/|2 EQU /s/:\(  *|2 \)/\1/
+# convert opcodes
+s/|2 ORG /|2 .org /
+s/|2 EQU /|2 .equ /
+s/|2 DB /|2 .db /
+s/|2 DW /|2 .dw /
+s/|2 END /|2 .end /
+' |tee x51.log |
+sed '
 # recover 
 s/|[123] //g
 s/|;/;/
